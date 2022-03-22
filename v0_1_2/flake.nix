12c995f2227c02457b7d76bc229c20bb1ba06a4e
hugosenari <hugosenari@gmail.com>
2022-03-22 01:10:00 -0300

    chore: re index htmlgenerator

---
+++ v0_1_2/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Generate HTML string by nim object'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-htmlgenerator-v0_1_2.flake = false;
+  inputs.src-htmlgenerator-v0_1_2.owner = "z-kk";
+  inputs.src-htmlgenerator-v0_1_2.ref   = "v0_1_2";
+  inputs.src-htmlgenerator-v0_1_2.repo  = "htmlgenerator";
+  inputs.src-htmlgenerator-v0_1_2.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-htmlgenerator-v0_1_2"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-htmlgenerator-v0_1_2";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
