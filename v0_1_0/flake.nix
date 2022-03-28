d2f0fb3b9d505a2760cda6341fe456cc02e136de
hugosenari <hugosenari@gmail.com>
2022-03-27 21:57:21 -0300

    feat(nimpkgs): remove lock

--- v0_1_0/flake.nix
+++ v0_1_0/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-htmlgenerator-v0_1_0.ref   = "refs/tags/v0.1.0";
   inputs.src-htmlgenerator-v0_1_0.owner = "z-kk";
   inputs.src-htmlgenerator-v0_1_0.repo  = "htmlgenerator";
-  inputs.src-htmlgenerator-v0_1_0.dir   = "";
   inputs.src-htmlgenerator-v0_1_0.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
--- v0_1_3/flake.nix
+++ v0_1_3/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-htmlgenerator-v0_1_3.ref   = "refs/tags/v0.1.3";
   inputs.src-htmlgenerator-v0_1_3.owner = "z-kk";
   inputs.src-htmlgenerator-v0_1_3.repo  = "htmlgenerator";
-  inputs.src-htmlgenerator-v0_1_3.dir   = "";
   inputs.src-htmlgenerator-v0_1_3.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
